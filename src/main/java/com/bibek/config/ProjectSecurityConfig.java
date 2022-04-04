93df7b60fa377e6ebdad0909476765fa4223f6a6
bibek <bibekshrestha101@gmail.com>
2022-04-03 23:38:53 -0400

    added cors config

--- src/main/java/com/bibek/config/ProjectSecurityConfig.java
+++ src/main/java/com/bibek/config/ProjectSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.bibek.config;
 
+import java.util.Collections;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
@@ -16,15 +19,32 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/myAccount").authenticated().antMatchers("/myBalance").authenticated()
-				.antMatchers("/myCards").authenticated().antMatchers("/myLoans").authenticated().antMatchers("/notices")
-				.permitAll().antMatchers("/contact").permitAll().and().formLogin().and().httpBasic();
+
+		http.cors().configurationSource(new CorsConfigurationSource() {
+			@Override
+			public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+				CorsConfiguration config = new CorsConfiguration();
+				config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+				config.setAllowedMethods(Collections.singletonList("*"));
+				config.setAllowCredentials(true);
+				config.setAllowedHeaders(Collections.singletonList("*"));
+				config.setMaxAge(3600L);
+				return config;
+			}
+		}).and().csrf().ignoringAntMatchers("/contact").csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and().
+		authorizeRequests().antMatchers("/myAccount").authenticated().antMatchers("/myBalance").authenticated()
+				.antMatchers("/myLoans").authenticated().antMatchers("/myCards").authenticated()
+				.antMatchers("/user").authenticated().antMatchers("/notices").permitAll()
+				.antMatchers("/contact").permitAll().and().httpBasic();
 
 		/*
 		 * The default behavior of spring security is to authenticate the request we can
--- src/main/java/com/bibek/controller/LoansController.java
+++ src/main/java/com/bibek/controller/LoansController.java
@@ -1,14 +1,30 @@
 package com.bibek.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bibek.model.Customer;
+import com.bibek.model.Loans;
+import com.bibek.repository.LoanRepository;
 
 @RestController
 public class LoansController {
 
+	@Autowired
+	private LoanRepository loanRepository;
 	
-	@GetMapping("/myLoans")
-	public String getLoan() {
-		return "from loan controller";
+	@PostMapping("/myLoans")
+	public List<Loans> getLoanDetails(@RequestBody Customer customer) {
+		List<Loans> loans = loanRepository.findByCustomerIdOrderByStartDtDesc(customer.getId());
+		if (loans != null ) {
+			return loans;
+		}else {
+			return null;
+		}
 	}
 }
